e99d981106816b3682ea16f0058c694c15e8c7bf
Alan dev <aland3v@gmail.com>
2021-02-16 09:36:18 -0400

    Implemented: Formulario de creacion de nuevo usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,19 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index() {
-
+        $users = User::latest()->get();
+        return view('users.index', [
+            'users' => $users
+        ]);
     }
 
-    public function store() {
-        
+    public function store(Request $request) {
+        $request->validate([
+            'name'  => 'required',
+            'email' => ['required','email','unique:users'],
+            'password' => ['required','min:8'],
+        ]);
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        return back(); // Retorna a la view anterior
     }
 
-    public function destroy() {
-        
+    public function destroy(User $user) {
+        $user->delete();
+        return back();
     }
 }
