238dcfbf963cd5b5141bc85c3c3823deb32c8598
Aland3v <aland3v@gmail.com>
2021-02-19 23:11:00 -0400

    Se adicionaron todos los templates y se desgloso los mismos con blade

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,49 @@ use App\Http\Controllers\UserController;
 */
 
 // Route::get('/', 'UserController@index');
-Route::get('/', 'ShopController@index');
+Route::get('/', 'ShopController@index')->name('index');
+
 Route::post('users', 'UserController@store')->name('users.store');
 Route::delete('users/{user}','UserController@destroy')->name('users.destroy');
+
+Route::get('/shop', function() {
+    return view('shop');
+})->name('shop');
+
+Route::get('/cart', function() {
+    return view('cart');
+})->name('cart');
+
+Route::get('/checkout', function() {
+    return view('checkout');
+})->name('checkout');
+
+Route::get('/blog-single', function() {
+    return view('blog-single');
+})->name('blog-single');
+
+Route::get('blog', function() {
+    return view('blog');
+})->name('blog');
+
+Route::get('404', function() {
+    return view('404');
+})->name('404');
+
+Route::get('contact-us', function() {
+    return view('contact-us');
+})->name('contact-us');
+
+Route::get('login', function() {
+    return view('login');
+})->name('login');
+
+
+Route::get('product-details', function() {
+    return view('product-details');
+})->name('product-details');
+
+
+Route::get('send-email', function() {
+    return view('send-email');
+})->name('send-email');
